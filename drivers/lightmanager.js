ecd85f95296e181c4127b18cba2300b874ab467c
Kevin Riemens <kevin.riemens@qnh.nl>
2016-12-28 18:44:05 +0100

    URI encoding only where neccessary

--- drivers/lightmanager.js
+++ drivers/lightmanager.js
@@ -18,19 +18,19 @@ class HttpDriver extends DriverBase {
     http.get({
       host: this.driver.url,
       port: this.driver.port,
-      path: encodeURIComponent('/cmd=' + device + ' ' + code + ' ' + address + ' LEARN ' + (state === 1) ? 'ON' : 'OFF')
+      path: '/cmd=' + encodeURIComponent(device + ' ' + code + ' ' + address + ' LEARN ' + (state === 1) ? 'ON' : 'OFF')
     }, function (response) {
       console.log(response);
     });
   }
 
   dim(device, code, address, level) {
-    console.log('do http request: ' + this.driver.url + ':' + this.driver.port + '/cmd=' + device + ' ' + code + ' ' + address + ' LEARN ' + state);
+    console.log('do http request: ' + this.driver.url + ':' + this.driver.port + '/cmd=' + device + ' ' + code + ' ' + address + ' LEARN ' + level);
 
     http.get({
       host: this.driver.url,
       port: this.driver.port,
-      path: '/cmd=' + device + ' ' + code + ' ' + address + ' LEARN ' + level
+      path: '/cmd=' + encodeURIComponent(device + ' ' + code + ' ' + address + ' LEARN ' + level
     }, function (response) {
       console.log(response);
     });
