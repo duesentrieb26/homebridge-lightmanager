1e867b189e9c83e971882d7e15eba6faf10890be
Kevin Riemens <kevin.riemens@qnh.nl>
2016-12-29 21:36:32 +0100

    Removed percentage conversion

--- drivers/lightmanager.js
+++ drivers/lightmanager.js
@@ -30,65 +30,13 @@ class HttpDriver extends DriverBase {
 
 
   dim(device, code, address, level) {
-    var percentage = '50%';
 
-    switch (level) {
-      case 0:
-        percentage = '0';
-        break;
-      case 1:
-        percentage = '12.50%';
-        break;
-      case 2:
-        percentage = '18.75%';
-        break;
-      case 3:
-        percentage = '25%';
-        break;
-      case 4:
-        percentage = '31.25%';
-        break;
-      case 5:
-        percentage = '37.50%';
-        break;
-      case 6:
-        percentage = '43.75%';
-        break;
-      case 7:
-        percentage = '50%';
-        break;
-      case 8:
-        percentage = '56.25%';
-        break;
-      case 9:
-        percentage = '62.50%';
-        break;
-      case 10:
-        percentage = '68.75%';
-        break;
-      case 11:
-        percentage = '75%';
-        break;
-      case 12:
-        percentage = '81.25%%';
-        break;
-      case 13:
-        percentage = '87.50%';
-        break;
-      case 14:
-        percentage = '93.75%';
-        break;
-      case 15:
-        percentage = '100%';
-        break;
-    }
-
-    console.log('do http request: ' + this.driver.url + ':' + this.driver.port + '/cmd=' + device + ' ' + code + ' ' + address + ' LEARN ' + percentage);
+    console.log('do http request: ' + this.driver.url + ':' + this.driver.port + '/cmd=' + device + ' ' + code + ' ' + address + ' LEARN ' + level);
 
     http.get({
       host: this.driver.url,
       port: this.driver.port,
-      path: '/cmd=' + encodeURIComponent(device + ' ' + code + ' ' + address + ' LEARN ' + percentage)
+      path: '/cmd=' + encodeURIComponent(device + ' ' + code + ' ' + address + ' LEARN ' + level)
     }, function (response) {
       console.log(response.statusCode + ' ' + response.statusMessage);
     });
