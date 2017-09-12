f36b0b0c97a3bcb851d7a51cfffa38e2a801e406
Kevin Riemens <kevin.riemens@qnh.nl>
2017-09-12 22:08:16 +0200

    variable declarations

--- drivers/lightmanager.js
+++ drivers/lightmanager.js
@@ -13,8 +13,11 @@ class HttpDriver extends DriverBase {
   }
 
   switch(device, code, address, state, learn) {
-    var onoff = (state === 1) ? 'ON' : 'OFF';
-    var learnable = (learn) ? ' LEARN ' : ' DIP ';
+
+    console.log('Switch ... ', device, code, address, state, learn);
+
+    let onoff = (state === 1) ? 'ON' : 'OFF';
+    let learnable = (learn) ? ' LEARN ' : ' DIP ';
 
     console.log('do http request: ' + this.driver.url + ':' + this.driver.port + '/cmd=' + device + ' ' + code + ' ' + address + learnable + onoff);
 
